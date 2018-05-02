2b42715ec653c46d0375dc094bec0ba46500da18
jianting <1727917318@qq.com>
2018-05-02 21:41:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/py/project14/AnotherActivity.java
@@ -0,0 +1,32 @@
+package com.example.py.project14;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Toast.makeText(AnotherActivity.this,"FFFFFF",Toast.LENGTH_LONG).show();
+        //System.out.println("eeee");
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                //Toast.makeText(AnotherActivity.this,"Do",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
